82e6f0a79a43fffef5e99d93f229de5fb9169b73
amgranad <ambergkim@gmail.com>
2017-10-04 20:55:05 -0700

    v3 try

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a,b){ //eslint-disable-line
-  return 'The sum of ' + a + ' and ' + b + ' is ' + (a + b);
+  var problem1Array = [];
+  problem1Array.push(a + b);
+  problem1Array.push('The sum of ' + a + ' and ' + b + ' is ' + (a + b));
+  return problem1Array;
 }
 
 // Here is the test for sum(); uncomment it to run it
@@ -27,14 +30,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a,b){ //eslint-disable-line
-  var numbers = [];
-  numbers.push(a * b);
-  numbers.push('The product of ' + a + ' and ' + b + ' is ' + (a * b));
-  return numbers;
+  // var numbers = [];
+  // numbers.push(a * b);
+  // numbers.push('The product of ' + a + ' and ' + b + ' is ' + (a * b));
+  // return numbers;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-multiply(5,9);
+// multiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -51,7 +54,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-
+  // var problem3Array = [];
+  // var element1a = sum(a, b);
+  // var element1b = sum(element1a, c);
+  // problem3Array.push(element1b);
+  // var element2a = multiply(a, b);
+  // var element2b = multiply(element2a, c);
+  // problem3Array.push(element2b);
+  // problem3Array.push(a + ' and ' + b + ' and ' + c + ' sum to ' + element1b);
+  // problem3Array.push('The product of ' + a + ' and ' + b + 'c' + ' is ' + element2b);
+  // return problem3Array;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
